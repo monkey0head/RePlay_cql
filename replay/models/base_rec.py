1c5429c130b6ea3fe81fb129e67bfc46900009a3
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2022-10-12 12:39:25 +0300

    fix

--- replay/models/base_rec.py
+++ replay/models/base_rec.py
@@ -357,7 +357,7 @@ class BaseRecommender(ABC):
         self.fit_items = sf.broadcast(items)
         self._num_users = self.fit_users.count()
         self._num_items = self.fit_items.count()
-        raise Exception(self.fit_users.agg({"user_idx": "max"}).collect())
+        raise Exception(self.fit_users.collect())
         self._user_dim_size = (
             self.fit_users.agg({"user_idx": "max"}).collect()[0][0] + 1
         )
