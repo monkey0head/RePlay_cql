75559972ea86a87d3523f1ad33e4dc08e3987330
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2022-10-12 15:59:31 +0300

    log

--- replay/models/cql.py
+++ replay/models/cql.py
@@ -117,6 +117,7 @@ class RLRecommender(Recommender):
     
     def __trajectory_preparator(self, log: DataFrame) -> MDPDataset:
         preparator_retail = RLDataPreparator(log.sort(['user_id','ts']))
+        raise Exception("Not in place!")
         observations, actions, rewards, termaits = preparator_retail.prepare_data(count_to_use = 1000)
         train_dataset = MDPDataset(
             observations=observations,
