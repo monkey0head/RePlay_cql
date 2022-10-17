1da264f76922a1cbb2afb52dd6624d5a8eb32367
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2022-10-17 12:05:50 +0300

    change to movie lens

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,1 +1,1 @@
-python compare_recs.py --ds Amazon.subscriptions --part 4 --mem 0.2 --algos sdac --epochs 3 --top --scale 0.01 --core 5 --label test --gpu 0
+python compare_recs.py --ds MovieLens.100k --part 4 --mem 0.2 --algos sdac --epochs 3 --top --scale 0.01 --core 5 --label test --gpu 0
