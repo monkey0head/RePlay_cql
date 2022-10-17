f24dca9e8bdbc2b4c33aec62e100e1271535e447
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2022-10-17 12:40:42 +0300

    fix borders

--- replay/models/base_rec.py
+++ replay/models/base_rec.py
@@ -151,13 +151,15 @@ class BaseRecommender(ABC):
             borders = search_space[param]["args"]
             param_type = search_space[param]["type"]
          #   raise Exception(value)
-            extra_category = (
-                param_type == "categorical" and value not in borders
-            )
-            param_out_of_bounds = param_type != "categorical" and (
-                value < borders[0] or value > borders[1]
-            )
-            
+            try:
+                extra_category = (
+                    param_type == "categorical" and value not in borders
+                )
+                param_out_of_bounds = param_type != "categorical" and (
+                    value < borders[0] or value > borders[1]
+                )
+            except:
+                raise Exception((value, borders))
             if extra_category or param_out_of_bounds:
                 outside_search_space[param] = {
                     "borders": borders,
