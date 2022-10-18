7ae774192fab71ec667d0b2dca355a83ca5c84bb
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2022-10-18 11:46:05 +0300

    fix run

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,1 +1,1 @@
-python compare_recs.py --ds MovieLens.1000k --part 4 --mem 0.2 --algos sdac --epochs 3 --top --scale 0.01 --core 5 --label test --gpu 0
+python compare_recs.py --ds MovieLens.1m --part 4 --mem 0.2 --algos sdac --epochs 3 --top --scale 0.01 --core 5 --label test --gpu 0
