1e1eeaa9b09bec7b339c30935db8e5f98e5dcff9
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2022-10-18 11:29:40 +0300

    more_epoches

--- replay/models/sdac/distributions.py
+++ replay/models/sdac/distributions.py
@@ -62,6 +62,7 @@ class GumbelDistribution(Distribution):
         a probability distribution that sums to 1 across classes.
         """
         y = self.logits + self.sample_gumbel()
+        raise Exception(self.temperature)
         out = torch.softmax(y / self.temperature, dim=-1)
         return out
 
