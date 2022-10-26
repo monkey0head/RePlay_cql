055c911fcb449160ca6db6096067dab1473acc19
zoya <zoya.v@ya.ru>
2022-10-26 22:03:25 +0800

    fix

--- experiments/pure_rl/fake_recommender_env.py
+++ experiments/pure_rl/fake_recommender_env.py
@@ -103,7 +103,7 @@ class FakeRecomenderEnv(gym.Env):
         try:
         	mask = self.log_data['user_id'] == self.episodes[self.episode_num]
         except:
-        	print(self.episode_num)
+        	print(self.episode_num, self.episodes)
         self.current_episode = self.log_data[mask]
        # print(self.current_episode['user_id'])
         self.user_hist.append(self.current_episode['user_id'].values[0])
