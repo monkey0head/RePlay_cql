57a394c702b872a01b152ef407b9718d88267522
zoya <zoya.v@ya.ru>
2022-11-01 15:28:31 +0800

    fix choosen of top_k

--- replay/models/fake_recommender_env.py
+++ replay/models/fake_recommender_env.py
@@ -73,7 +73,7 @@ class FakeRecomenderEnv(gym.Env):
             ndcg_ = ndcg( self.top_k, pred_top_k['item_hist'].values, self.original['item_idx'].values)
             mape_ = mape( self.top_k, pred_top_k['item_hist'].values, self.original['item_idx'].values)
             
-            print(pred_top_k['item_hist'].values, self.original['item_idx'].values, ndcg_)
+           # print(pred_top_k['item_hist'].values, self.original['item_idx'].values, ndcg_)
             wandb.log({"episode": self.total_episodes, "NDCG": ndcg_, "MAP": mape_})
             self.total_ndsg.append(ndcg_)
             self.total_mape.append(mape_)
