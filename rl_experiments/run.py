7b1c4b20f6f5a93540cf76264b88d7e304b71070
Petr Kuderov <pkuderov@gmail.com>
2022-11-08 11:07:29 +0300

    undo relative module imports

--- rl_experiments/run.py
+++ rl_experiments/run.py
@@ -1,5 +1,5 @@
-from .run.entrypoint import run_experiment, get_run_command_arg_parser
-from .scenarios.test_pipeline import TestPipelineExperiment
+from run.entrypoint import run_experiment, get_run_command_arg_parser
+from scenarios.test_pipeline import TestPipelineExperiment
 
 
 if __name__ == "__main__":
