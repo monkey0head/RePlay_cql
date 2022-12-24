b8710de5f64b78d1801449f6cfb26e3c9bff0ab8
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2022-12-24 14:32:54 +0300

    cold users in test problem

--- replay/models/rl/experiments/compare_recs.py
+++ replay/models/rl/experiments/compare_recs.py
@@ -130,12 +130,12 @@ class RatingsDataset:
         self.test_users = test_users
         
         train_users = raw_train.select('user_idx').distinct().cache().toPandas()
-        test_users = raw_test.toPandas()
+        test_users = raw_test.select('user_idx').distinct().cache().toPandas()
        # print(train_users)
        # print(test_users)
         for user in list(test_users.values):
-            print(user)
-            if user[0] not in list(train_users.values):
+            #print(user)
+            if user not in list(train_users.values):
                 print(user)
                 print("Aboba!")
             
