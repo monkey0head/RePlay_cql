61b53d0510a9fedb575d9deeda64c068859a8a04
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2023-01-16 11:12:34 +0300

    fix n epoches

--- replay/models/rl/rl_recommender.py
+++ replay/models/rl/rl_recommender.py
@@ -155,8 +155,8 @@ class RLRecommender(Recommender):
             self.fitter = self.model.fitter(
                 self.train,
                # n_epochs=self.n_epochs,
-                n_steps = 200*self.n_epochs,
-                n_steps_per_epoch = 200,
+                n_steps = 2000*self.n_epochs,
+                n_steps_per_epoch = 2000,
                 # eval_episodes=self.train,
                 # scorers={'environment': evaluate_scorer}
             )
