5a6da351c8100c4657d07efe9d25fc498e089480
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2023-01-19 15:54:01 +0300

    fix rat form

--- replay/models/rl/rating_mdp/mdp/rating_mdp.py
+++ replay/models/rl/rating_mdp/mdp/rating_mdp.py
@@ -23,6 +23,16 @@ def negative_reward_binary_actions(df):
 def negative_reward(df):
     reward = df['rating'].to_numpy().astype(np.int)
     actions = reward.copy()
+    
   #  reward[reward<4] = -1
     reward[:] = 1
+    
+    reward_cp = reward.copy()
+    reward_cp = -1
+    
+    actions_cp = actions.copy()
+    actions_cp = np.abs(actions_cp - 5)
+    
+    reward = np.append(reward, reward_cp)
+    actions = np.append(actions, actions_cp)
     return reward, actions
