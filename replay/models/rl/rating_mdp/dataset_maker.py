d7e7664207441bd8ecaa798ccf805906d7afe266
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2023-01-19 14:36:59 +0300

    make_dataset_script

---
+++ replay/models/rl/rating_mdp/dataset_maker.py
@@ -0,0 +1,8 @@
+import pandas as pd
+train_df = pd.read_csv("../experiments/train_df.csv", sep = "\t")
+test_df = pd.read_csv("../experiments/test_df.csv", sep = "\t")
+train_df.loc[:,"dataset"]="train"
+test_df.loc[:,"dataset"]="test"
+df = train_df.append(test_df, ignore_index=True)
+df = df.rename(columns={"user_idx": "user_id", "item_idx": "item_id", "relevance":"rating"})
+df.to_csv("ml_prepared.csv")
