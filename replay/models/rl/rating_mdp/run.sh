78f07e9cd1508b0861abfb969b137c6aba1a229c
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2023-01-19 14:24:28 +0300

    fix run module

--- replay/models/rl/rating_mdp/run.sh
+++ replay/models/rl/rating_mdp/run.sh
@@ -1,3 +1,3 @@
-python3 replay/models/rl/rating_mdp/rating_experiment.py --top_k 10 --emb als --data_use 90000 --epochs 100 --pfunc o --wandb_run_name random_10000_top10_p2_o_repdat
+python3 rating_experiment.py --top_k 10 --emb als --data_use 90000 --epochs 100 --pfunc o --wandb_run_name random_10000_top10_p2_o_repdat
 
 
