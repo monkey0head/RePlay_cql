3442d2ce02b1a455332b285260596cac3e422fdc
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2023-01-19 14:25:24 +0300

    remove sdac

--- replay/models/rl/rating_mdp/rating_experiment.py
+++ replay/models/rl/rating_mdp/rating_experiment.py
@@ -8,7 +8,7 @@ import d3rlpy
 from d3rlpy.base import LearnableBase
 from d3rlpy.dataset import MDPDataset
 from d3rlpy.metrics import evaluate_on_environment
-from d3rlpy.algos import DiscreteCQL, DiscreteSAC, SDAC
+from d3rlpy.algos import DiscreteCQL, DiscreteSAC#, SDAC
 
 import rs_datasets
 import argparse
