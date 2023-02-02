64014b0c7d2a4ada9049c9f1822a6e81502a096f
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2023-02-02 12:26:59 +0300

    scripts for running ML experiments

---
+++ replay/models/rl/experiments/ml_experiments.sh
@@ -0,0 +1,4 @@
+#python compare_recs.py --ds MovieLens.100k --part 4 --mem 0.2 --algos cql als knn ddpg --epochs 10 20 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 105 110 115 120 125 --rew neg --label test --test_ratio 0.2
+
+
+python compare_recs.py --ds MovieLens.1m --part 4 --mem 0.2 --algos cql als knn ddpg --epochs 10 20 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 105 110 115 120 125 --rew neg --label test --test_ratio 0.01
