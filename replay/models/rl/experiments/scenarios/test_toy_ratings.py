c10460eb35b382869ba1991bb2018d6561f8419d
Petr Kuderov <pkuderov@gmail.com>
2023-02-17 08:38:32 +0300

    support several rl models, train-test split config, augmentations

--- replay/models/rl/experiments/scenarios/test_toy_ratings.py
+++ replay/models/rl/experiments/scenarios/test_toy_ratings.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import dataclasses
 import logging
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -10,6 +11,7 @@ from d3rlpy.dataset import MDPDataset
 from numpy.random import Generator
 
 from replay.models.rl.experiments.datasets.toy_ratings import generate_clusters
+from replay.models.rl.experiments.run.wandb import get_logger
 from replay.models.rl.experiments.utils.config import (
     TConfig, GlobalConfig, LazyTypeResolver
 )
@@ -25,20 +27,27 @@ class RandomLogGenerator:
     n_items: int
     n_pairs: int
 
-    def __init__(self, seed: int, n_users: int, n_items: int, n_pairs: int | float):
+    def __init__(
+            self, seed: int, n_users: int, n_items: int, n_pairs: int | float,
+            duplicates: bool = False
+    ):
         self.rng = np.random.default_rng(seed)
         self.n_users = n_users
         self.n_items = n_items
-        self.n_pairs = n_pairs if isinstance(n_pairs, int) else int(n_pairs * n_users * n_items)
+        self.n_pairs = self._to_pairs(n_pairs)
+        self.duplicates = duplicates
 
-    def generate(self) -> pd.DataFrame:
+    def generate(self, n_pairs: int | float = None, duplicates: bool = None) -> pd.DataFrame:
+        n_pairs = self._to_pairs(n_pairs) if n_pairs is not None else self.n_pairs
+        duplicates = duplicates if duplicates is not None else self.duplicates
+
         log_pairs = self.rng.choice(
             self.n_users * self.n_items,
-            size=self.n_pairs,
-            replace=False
+            size=n_pairs,
+            replace=duplicates
         )
         # timestamps denote the order of interactions, this is not the real timestamp
-        timestamps = np.arange(self.n_pairs)
+        timestamps = self.rng.uniform(size=n_pairs)
         log_users, log_items = np.divmod(log_pairs, self.n_items)
         log = pd.DataFrame({
             'user_id': log_users,
@@ -51,6 +60,9 @@ class RandomLogGenerator:
             ascending=[True, False]
         )
         return log
+
+    def _to_pairs(self, n_pairs: int | float):
+        return n_pairs if isinstance(n_pairs, int) else int(n_pairs * self.n_users * self.n_items)
 
 
 class RandomEmbeddingsGenerator:
@@ -105,13 +117,14 @@ class RandomClustersEmbeddingsGenerator:
         return np.clip(0, 1, embedding)
 
 
-class ToyRatingsGenerator:
+class RelevanceCalculator:
     metric: str
     positive_ratio: float
 
     def __init__(self, metric: str, positive_ratio: float):
         self.metric = metric
         self.positive_ratio = positive_ratio
+        self.relevant_threshold = None
 
     def similarity(self, users: np.ndarray, items: np.ndarray) -> np.ndarray | float:
         if self.metric == 'l1':
@@ -127,49 +140,24 @@ class ToyRatingsGenerator:
             items_norm = np.linalg.norm(items, axis=-1)
             return dot_product / (users_norm * items_norm)
 
-    def generate(self, users: np.ndarray, items: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    def calculate(self, users: np.ndarray, items: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         similarity = self.similarity(users, items)
-        relevant_threshold = np.quantile(similarity, 1 - self.positive_ratio, interpolation='lower')
-        relevant = similarity >= relevant_threshold
+        if self.relevant_threshold is None:
+            self.relevant_threshold = np.quantile(
+                similarity, 1 - self.positive_ratio, interpolation='lower'
+            )
+        relevant = similarity >= self.relevant_threshold
 
-        continuous_ratings = similarity
-        discrete_ratings = relevant
-        return continuous_ratings, discrete_ratings
+        continuous_relevance = similarity
+        discrete_relevance = relevant.astype(int)
+        return continuous_relevance, discrete_relevance
 
 
+@dataclasses.dataclass
 class ToyRatingsDataset:
     log: pd.DataFrame
     user_embeddings: np.ndarray
     item_embeddings: np.ndarray
-
-    def __init__(
-            self, global_config: GlobalConfig, source: TConfig,
-            embeddings_n_dims: int, user_embeddings: TConfig, item_embeddings: TConfig,
-            ratings: TConfig
-    ):
-        self.global_config = global_config
-        self.source = global_config.resolve_object(source)
-        self.embeddings_n_dims = embeddings_n_dims
-
-        user_embeddings_generator = global_config.resolve_object(
-            user_embeddings, n_dims=embeddings_n_dims
-        )
-        self.user_embeddings = user_embeddings_generator.generate(self.source.n_users)
-
-        item_embeddings_generator = global_config.resolve_object(
-            item_embeddings, n_dims=embeddings_n_dims
-        )
-        self.item_embeddings = item_embeddings_generator.generate(self.source.n_items)
-
-        self.ratings = ToyRatingsGenerator(**ratings)
-
-    def generate(self):
-        self.log = self.source.generate()
-        continuous_ratings, discrete_ratings = self.ratings.generate(
-            self.log_user_embeddings, self.log_item_embeddings
-        )
-        self.log['continuous_rating'] = continuous_ratings
-        self.log['discrete_rating'] = discrete_ratings
 
     @property
     def log_user_embeddings(self):
@@ -187,6 +175,75 @@ class ToyRatingsDataset:
     def log_discrete_ratings(self):
         return self.log['discrete_rating']
 
+    @property
+    def log_ground_truth(self):
+        return self.log['ground_truth']
+
+
+class ToyRatingsDatasetBuilder:
+    user_embeddings: np.ndarray
+    item_embeddings: np.ndarray
+
+    def __init__(
+            self, global_config: GlobalConfig, seed: int, source: TConfig,
+            embeddings_n_dims: int, user_embeddings: TConfig, item_embeddings: TConfig,
+            ratings: TConfig
+    ):
+        self.global_config = global_config
+        self.rng = np.random.default_rng(seed)
+        self.source = global_config.resolve_object(source)
+        self.embeddings_n_dims = embeddings_n_dims
+
+        user_embeddings_generator = global_config.resolve_object(
+            user_embeddings, n_dims=embeddings_n_dims
+        )
+        item_embeddings_generator = global_config.resolve_object(
+            item_embeddings, n_dims=embeddings_n_dims
+        )
+        self.user_embeddings = user_embeddings_generator.generate(self.source.n_users)
+        self.item_embeddings = item_embeddings_generator.generate(self.source.n_items)
+        self.relevance = RelevanceCalculator(**ratings)
+
+    def generate(self) -> ToyRatingsDataset:
+        dataset = ToyRatingsDataset(
+            self.source.generate(),
+            user_embeddings=self.user_embeddings, item_embeddings=self.item_embeddings
+        )
+        continuous_ratings, discrete_ratings = self.relevance.calculate(
+            dataset.log_user_embeddings, dataset.log_item_embeddings
+        )
+        dataset.log['continuous_rating'] = continuous_ratings
+        dataset.log['discrete_rating'] = discrete_ratings
+        dataset.log['gt_continuous_rating'] = continuous_ratings
+        dataset.log['gt_discrete_rating'] = discrete_ratings
+        dataset.log['ground_truth'] = True
+        return dataset
+
+    def generate_augmentations(self, n_pairs: int | float) -> ToyRatingsDataset:
+        dataset = ToyRatingsDataset(
+            self.source.generate(n_pairs=n_pairs, duplicates=True),
+            user_embeddings=self.user_embeddings, item_embeddings=self.item_embeddings
+        )
+        continuous_ratings, discrete_ratings = self.relevance.calculate(
+            dataset.log_user_embeddings, dataset.log_item_embeddings
+        )
+        rand_order = self.rng.permutation(continuous_ratings.shape[0])
+        dataset.log['continuous_rating'] = continuous_ratings[rand_order]
+        dataset.log['discrete_rating'] = discrete_ratings[rand_order]
+        dataset.log['gt_continuous_rating'] = continuous_ratings
+        dataset.log['gt_discrete_rating'] = discrete_ratings
+        dataset.log['ground_truth'] = False
+        return dataset
+
+    def split(self, dataset: ToyRatingsDataset, split_by, train: float) -> ToyRatingsDataset:
+        n_train_users = int(train * self.source.n_users)
+        train_users = self.rng.choice(self.source.n_users, size=n_train_users, replace=False)
+        return ToyRatingsDataset(
+            dataset.log[dataset.log['user_id'].isin(train_users)].copy(),
+            user_embeddings=self.user_embeddings,
+            item_embeddings=self.item_embeddings
+        )
+
 
 class MdpDatasetBuilder:
     actions: str
@@ -196,7 +253,7 @@ class MdpDatasetBuilder:
         self.actions = actions
         self.rewards = rewards
 
-    def build(self, ds):
+    def build(self, ds: ToyRatingsDataset) -> MDPDataset:
         observations = np.concatenate((ds.log_user_embeddings, ds.log_item_embeddings), axis=-1)
         actions = self._get_actions(ds)
         rewards = self._get_rewards(ds)
@@ -213,14 +270,32 @@ class MdpDatasetBuilder:
         rewards = np.zeros(ds.log.shape[0])
         for name, value in self.rewards.items():
             if name == 'baseline':
-                baseline: float = value
-                rewards += np.full_like(rewards, baseline)
+                baseline = np.array(value)
+                rewards += baseline[ds.log_ground_truth.values.astype(int)]
             elif name == 'continuous':
                 weight: float = value
-                rewards += ds.log_continuous_ratings.values * weight
+                rewards[ds.log_ground_truth] += (
+                        ds.log_continuous_ratings[ds.log_ground_truth] * weight
+                )
             elif name == 'discrete':
                 weights = np.array(value)
-                rewards = weights[ds.log_discrete_ratings.values.astype(int)]
+                rewards[ds.log_ground_truth] = weights[
+                    ds.log_discrete_ratings[ds.log_ground_truth]
+                ]
+            elif name == 'continuous_error':
+                weight: float = value
+                err = np.abs(
+                    ds.log_continuous_ratings[~ds.log_ground_truth]
+                    - ds.log[~ds.log_ground_truth]['gt_continuous_rating']
+                )
+                rewards[~ds.log_ground_truth] -= err * weight
+            elif name == 'discrete_error':
+                weight: float = value
+                err = np.abs(
+                    ds.log_discrete_ratings[~ds.log_ground_truth]
+                    - ds.log[~ds.log_ground_truth]['gt_discrete_rating']
+                )
+                rewards[~ds.log_ground_truth] -= err * weight
         return rewards
 
     def _get_actions(self, ds):
@@ -248,69 +323,122 @@ class ToyRatingsExperiment:
 
     top_k: int
     epochs: int
+    eval_schedule: int
 
     def __init__(
             self, config: TConfig, config_path: Path, seed: int,
-            top_k: int, epochs: int, dataset: TConfig, mdp: TConfig,
+            top_k: int, epochs: int, dataset: TConfig, mdp: TConfig, model: TConfig,
+            train_test_split: TConfig, augmentations: TConfig,
+            log: bool, project: str, eval_schedule: int,
             cuda_device: bool | int | None,
             **_
     ):
         self.config = GlobalConfig(
             config=config, config_path=config_path, type_resolver=TypesResolver()
         )
+        self.logger = get_logger(config, log=log, project=project)
 
         self.init_time = timer()
         self.print_with_timestamp('==> Init')
 
-        if cuda_device is not None:
-            import torch.cuda
-            print(f'CUDA available: {torch.cuda.is_available()}')
-
+        self.cuda_device = get_cuda_device(cuda_device)
         self.seed = seed
         self.top_k = top_k
         self.epochs = epochs
+        self.eval_schedule = eval_schedule
 
-        self.dataset = self.config.resolve_object(dataset)
-        self.dataset.generate()
-        self.train_mdp = MdpDatasetBuilder(**mdp).build(self.dataset)
+        self.dataset_generator = self.config.resolve_object(dataset)
+        full_dataset = self.dataset_generator.generate()
+        train_dataset = self.dataset_generator.split(full_dataset, **train_test_split)
+        augmentations = self.dataset_generator.generate_augmentations(**augmentations)
+        train_log = pd.concat([train_dataset.log, augmentations.log], ignore_index=True)
+        train_log.sort_values(
+            ['user_id', 'timestamp'],
+            inplace=True,
+            ascending=[True, False]
+        )
+
+        mdp_builder = MdpDatasetBuilder(**mdp)
+        self.test_mdp = mdp_builder.build(full_dataset)
+        self.train_mdp = mdp_builder.build(ToyRatingsDataset(
+            log=train_log,
+            user_embeddings=train_dataset.user_embeddings,
+            item_embeddings=train_dataset.item_embeddings,
+        ))
+        self.model = self.config.resolve_object(model)
 
     def run(self):
         logging.disable(logging.DEBUG)
+        self.set_metrics()
 
         self.print_with_timestamp('==> Run')
-        from d3rlpy.algos import CQL
-        cql = CQL(
-            use_gpu=False, batch_size=16,
-            # actor_learning_rate=1e-3, critic_learning_rate=3e-3,
-        )
-        fitter = cql.fitter(
+        fitter = self.model.fitter(
             self.train_mdp,
             n_epochs=self.epochs, verbose=False,
             save_metrics=False, show_progress=False,
         )
         for epoch, metrics in fitter:
-            if epoch == 1 or epoch % 10 == 0:
-                self._eval_and_print(cql, epoch)
+            if epoch == 1 or epoch % self.eval_schedule == 0:
+                self._eval_and_log(self.model, epoch)
+            elif self.logger:
+                self.logger.log({'epoch': epoch})
 
         self.print_with_timestamp('<==')
 
-    def _eval_and_print(self, model, epoch):
-        train_prediction = model.predict(self.train_mdp.observations)
-        train_loss = np.mean(np.abs(train_prediction - self.train_mdp.actions))
-        self.print_with_timestamp(f'Epoch {epoch}: train loss = {train_loss:.4f}')
+    def _eval_and_log(self, model, epoch):
+        test_prediction = model.predict(self.test_mdp.observations)
+        test_loss = np.mean(np.abs(test_prediction - self.test_mdp.actions))
+        self.print_with_timestamp(f'Epoch {epoch}: test loss = {test_loss:.4f}')
+        if self.logger:
+            self.logger.log({
+                'epoch': epoch,
+                'mae': test_loss,
+            })
 
     def print_with_timestamp(self, text: str):
         print_with_timestamp(text, self.init_time)
+
+    def set_metrics(self):
+        if not self.logger:
+            return
+
+        self.logger.define_metric('epoch')
+        self.logger.define_metric('mae', step_metric='epoch')
 
 
 class TypesResolver(LazyTypeResolver):
     def resolve(self, type_name: str, **kwargs):
         if type_name == 'dataset.toy_ratings':
-            return ToyRatingsDataset
+            return ToyRatingsDatasetBuilder
         if type_name == 'ds_source.random':
             return RandomLogGenerator
         if type_name == 'embeddings.random':
             return RandomEmbeddingsGenerator
         if type_name == 'embeddings.clusters':
             return RandomClustersEmbeddingsGenerator
+        if type_name == 'd3rlpy.cql':
+            from d3rlpy.algos import CQL
+            return CQL
+        if type_name == 'd3rlpy.sac':
+            from d3rlpy.algos import SAC
+            return SAC
+        if type_name == 'd3rlpy.discrete_cql':
+            from d3rlpy.algos import DiscreteCQL
+            return DiscreteCQL
+        if type_name == 'd3rlpy.sdac':
+            from replay.models.rl.sdac.sdac import SDAC
+            return SDAC
+        if type_name == 'd3rlpy.discrete_sac':
+            from d3rlpy.algos import DiscreteSAC
+            return DiscreteSAC
         raise ValueError(f'Unknown type: {type_name}')
+
+
+def get_cuda_device(cuda_device: int | None) -> int | bool:
+    if cuda_device is not None:
+        import torch.cuda
+        cuda_available = torch.cuda.is_available()
+        print(f'CUDA available: {cuda_available}')
+        if not cuda_available:
+            cuda_device = False
+    return cuda_device
