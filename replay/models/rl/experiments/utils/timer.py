48884c821fa1b09112f879e66057da760a67d2ea
Petr Kuderov <pkuderov@gmail.com>
2023-03-03 09:30:55 +0300

    improve timer time printing format

--- replay/models/rl/experiments/utils/timer.py
+++ replay/models/rl/experiments/utils/timer.py
@@ -4,4 +4,13 @@ timer = timeit.default_timer
 
 
 def print_with_timestamp(text: str, start_time: float):
-    print(f'[{timer() - start_time:5.1f}] {text}')
+    elapsed_sec = timer() - start_time
+    if elapsed_sec < 1:
+        time_format = '5.3f'
+    elif elapsed_sec < 10:
+        time_format = '5.2f'
+    elif elapsed_sec < 1000:
+        time_format = '5.1f'
+    else:
+        time_format = '5.0f'
+    print(f'[{elapsed_sec:{time_format}}] {text}')
